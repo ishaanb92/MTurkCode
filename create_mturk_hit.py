eeb1e25697db54a2e69a7c0e879ed299023702fb
kilgore92 <ishaan.bits92@gmail.com>
2017-12-09 14:42:43 +0100

    Refactor code -- parametrize and separate into small and simple functions

--- create_mturk_hit.py
+++ create_mturk_hit.py
@@ -1,41 +1,81 @@
 #!/usr/bin/env python3
 import boto3
 import pickle
+import generate_hit
 
-MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+def create_hit(mturk,question):
+    """
+    Creates a single HIT using the MTurk object and the supplied XML-HTML question form
+    Returns the ID of the created HIT
 
-mturk = boto3.client('mturk',
-                    aws_access_key_id = "AKIAI4DQ22LGP4HC3X4Q",
-                    aws_secret_access_key = "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",
-                    region_name='us-east-1',
-                    endpoint_url = MTURK_SANDBOX
-                    )
+    """
 
-print ("I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account")
+    new_hit = mturk.create_hit(
+        Title = 'Finding similar images',
+        Description = 'Given are sets of 3 images, select which image looks more like the original image',
+        Keywords = 'text, quick, labeling',
+        Reward = '0.01',
+        MaxAssignments = 1,
+        LifetimeInSeconds = 172800,
+        AssignmentDurationInSeconds = 600,
+        AutoApprovalDelayInSeconds = 14400,
+        Question = question
+    )
 
-hit_list = [] #Haha
-with open('test.xml','r') as f:
-    test_hit = f.read()
+    return new_hit['HIT']
 
-new_hit = mturk.create_hit(
-    Title = 'Finding similar images',
-    Description = 'Given are sets of 3 images, select which image looks more like the original image',
-    Keywords = 'text, quick, labeling',
-    Reward = '0.01',
-    MaxAssignments = 1,
-    LifetimeInSeconds = 172800,
-    AssignmentDurationInSeconds = 10*60*60,
-    AutoApprovalDelayInSeconds = 14400,
-    Question = test_hit
-)
+def create_question(url_struct,question_num,num_images,pair_num):
+    """
+    Creates HTML (with XML wrapper) question form needed for a single HIT
 
-hit_list.append(new_hit['HIT']['HITId'])
+    """
+    merged_questions = ""
+    images_per_hit = []
+    for image_num in range(question_num,question_num+num_images):
+        image_triple = []
+        merged_questions += generate_hit.generate_single_question(url_struct[image_num][0],url_struct[image_num][pair_num][0],url_struct[image_num][pair_num][1])
+        image_triple.append(url_struct[image_num][0])
+        image_triple.append(url_struct[image_num][pair_num][0])
+        image_triple.append(url_struct[image_num][pair_num][1])
+        images_per_hit.append(image_triple)
 
-print ("A new HIT has been created. You can preview it here:")
-print ("https://workersandbox.mturk.com/mturk/preview?groupId=" + new_hit['HIT']['HITGroupId'])
-print ("HITID = " + new_hit['HIT']['HITId'] + " (Use to Get Results)")
+    question = generate_hit.generate_html_question(merged_questions = merged_questions)
 
-#Save the list of hit IDs
-with open('hits.pkl','wb') as f:
-    pickle.dump(hit_list,f)
+    return question,images_per_hit
+
+def get_url_struct(filename):
+
+    """
+    Returns struct containing all image URLs
+
+    """
+    with open(filename,'rb') as f:
+        url_struct = pickle.load(f)
+    return url_struct
+
+if __name__ == '__main__':
+
+    MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+
+    mturk = boto3.client('mturk',
+                        aws_access_key_id = "AKIAI4DQ22LGP4HC3X4Q",
+                        aws_secret_access_key = "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",
+                        region_name='us-east-1',
+                        endpoint_url = MTURK_SANDBOX
+                        )
+
+    print ("I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account")
+    hit_dict = {}
+    url_struct = get_url_struct(filename = 'url_struct.pkl')
+    question,images_per_hit = create_question(url_struct,question_num = 0,num_images = 10, pair_num = 1)
+    with open('debug.xml','w') as f:
+        f.write(question)
+
+    hit = create_hit(mturk=mturk,question=question)
+    hit_dict[hit['HITId']] = images_per_hit
+    print ("A new HIT has been created. You can preview it here:")
+    print ("https://workersandbox.mturk.com/mturk/preview?groupId=" + hit['HITGroupId'])
+    print ("HITID = " + hit['HITId']+ " (Use to Get Results)")
+    print(hit_dict)
+
 
--- generate_hit.py
+++ generate_hit.py
@@ -1,75 +1,78 @@
-import pickle
+def generate_single_question(original,image1,image2):
+    """
+    Generates a single question i.e. for a given original image
+    and a pair of generated images
 
-upper_const_str = """
-    <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
-     <HTMLContent><![CDATA[
+    Returns the question string
 
-     <!-- YOUR HTML BEGINS -->
-     <!DOCTYPE html>
-     <html>
-     <head>
-     <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
-     <script type='text/javascript' src='https://s3.amazonaws.com/mturk-public/externalHIT_v1.js'></script>
-     </head>
-     <body>
-     <form name='mturk_form' method='post' id='mturk_form' action='https://www.mturk.com/mturk/externalSubmit'><input type='hidden' value='' name='assignmentId'                id='assignmentId'/>
-     <p>Select the between A and B, the image that seems more similar to the original, if unsure, select the "Unsure" option :</p>
     """
 
-lower_const_string = """
-   <div>
-     <button type="submit">Submit</button>
-   </div>
-</form>
- <script language='Javascript'>turkSetAssignmentID();</script>
- </body></html>
- <!-- YOUR HTML ENDS -->
- ]]>
-</HTMLContent>
+    question = """
+       <div>
+         <img src="{0}" alt="Original" width="64" height="64">
+         <p> Original </p>
 
-<FrameHeight>600</FrameHeight>
+         <img src="{1}" alt="Image A" width="64" height="64">
+         <p> Image A </p>
 
-</HTMLQuestion>
-"""
+         <img src="{2}" alt="Image B" width="64" height="64">
+         <p> Image B </p>
+         <input type="radio" id="{1}" name="{0}" value="{1}">
+         <label for="{1}">Image A</label>
 
-if __name__ == "__main__":
-    with open('url_struct.pkl','rb') as f:
-        url_struct = pickle.load(f)
+         <input type="radio" id="{2}" name="{0}" value="{2}">
+         <label for="{2}">Image B</label>
 
-    final_xml = "" + upper_const_str
-    for i in range(10):
-        # This is one "question"
-        question = """
-           <div>
-             <img src="{0}" alt="Original" width="64" height="64">
-             <p> Original </p>
+         <input type="radio" id="{0}" name="{0}" value="Unsure">
+         <label for="{0}">Unsure</label>
+       </div>
+       <!-- 1 question ends -->
+    """.format(
+        original,
+        image1,
+        image2)
 
-             <img src="{1}" alt="Image A" width="64" height="64">
-             <p> Image A </p>
+    return question
 
-             <img src="{2}" alt="Image B" width="64" height="64">
-             <p> Image B </p>
-             <input type="radio" id="{1}" name="{0}" value="{1}">
-             <label for="{1}">Image A</label>
+def generate_html_question(merged_questions):
+    """
+    Generates the final XML needed to create HIT
 
-             <input type="radio" id="{2}" name="{0}" value="{2}">
-             <label for="{2}">Image B</label>
+    Returns the generated XML as a string
 
-             <input type="radio" id="{0}" name="{0}" value="Unsure">
-             <label for="{0}">Unsure</label>
-           </div>
-           <!-- 1 question ends -->
-        """.format(
-            url_struct[i][0],
-            url_struct[i][1][0],
-            url_struct[i][1][1])
+    """
+    upper_const_str = """
+        <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
+         <HTMLContent><![CDATA[
 
-        final_xml = final_xml + question
+         <!-- YOUR HTML BEGINS -->
+         <!DOCTYPE html>
+         <html>
+         <head>
+         <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
+         <script type='text/javascript' src='https://s3.amazonaws.com/mturk-public/externalHIT_v1.js'></script>
+         </head>
+         <body>
+         <form name='mturk_form' method='post' id='mturk_form' action='https://www.mturk.com/mturk/externalSubmit'><input type='hidden' value='' name='assignmentId'                id='assignmentId'/>
+         <p>Select the between A and B, the image that seems more similar to the original, if unsure, select the "Unsure" option :</p>
+        """
 
+    lower_const_str = """
+       <div>
+         <button type="submit">Submit</button>
+       </div>
+    </form>
+     <script language='Javascript'>turkSetAssignmentID();</script>
+     </body></html>
+     <!-- YOUR HTML ENDS -->
+     ]]>
+    </HTMLContent>
 
+    <FrameHeight>600</FrameHeight>
 
-    final_xml = final_xml + lower_const_string
-    #Write to file
-    with open('test.xml','w') as f:
-        f.write(final_xml)
+    </HTMLQuestion>
+    """
+    final_xml = "" + upper_const_str + merged_questions + lower_const_str
+
+    return final_xml
 
